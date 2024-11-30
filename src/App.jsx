575b320c79c8756bb8997b5204f89848eeb09e4e
Atharv Raut <atharvgraut@gmail.com>
2024-11-30 20:48:33 +0530

    LoginPage Added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Home from './Pages/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
-// In your App.js or index.js
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import AboutUs from './Pages/AboutUs';
+import LoginPage from './Pages/LoginPage';
 import Footer from './Components/Footer';
-
 
 const App = () => {
   return (
@@ -17,6 +16,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<AboutUs/>}/>
+      <Route path="/login" element={<LoginPage/>} />
     </Routes>
     <Footer/>
      
@@ -24,4 +24,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App 
---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './LoginPage.css';
+
+const LoginPage = () => {
+  return (
+    <div className="login-container">
+      <div className="login-content">
+        {/* Image Section */}
+        <div className="image-section">
+          <img
+            src="/big-five-personality.webp" 
+            alt="Welcome"
+            className="welcome-image"
+          />
+        </div>
+
+        {/* Form Section */}
+        <div className="form-section">
+          <h1>🧠<br/> BIG 5<br/>PERSONALITY TEST</h1>
+          <h2>Login</h2>
+          <form>
+            {/* Username Input */}
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Username"
+                className="input-box"
+                required
+              />
+              <span className="icon user-icon">👤</span>
+            </div>
+
+            {/* Password Input */}
+            <div className="input-group">
+              <input
+                type="password"
+                placeholder="Password"
+                className="input-box"
+                required
+              />
+              <span className="icon lock-icon">🔒</span>
+            </div>
+
+            {/* Login Button */}
+            <button type="submit" className="login-button">
+              <i className="button-icon">🔑</i> Login
+            </button>
+
+            {/* Links */}
+            <div className="links">
+              <a href="#" className="forgot-password">Forgot Password?</a>
+              <a href="#" className="register-link">New user? Register here</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
