739d42384209d9763b767f825814e60d526d1c5c
grpansare <grpansare2002@gmail.com>
2025-01-03 18:15:55 +0530

    al

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/big_5_personality_frontend/'
+  base:'/big_5_personality_frontend/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Ensure assets are placed here
+  },
 })
